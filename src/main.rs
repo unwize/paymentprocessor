a0dfdadbe67d7fa4c66722854e632d658779ac78
Unwize <aaronsigaldev@gmail.com>
2025-09-27 19:02:22 -0700

    Modified csv reader to use hand-written schema and ignore header row

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,13 @@ mod structures;
 
 use crate::errors::KrakenError;
 use crate::errors::KrakenError::Error;
+use crate::structures::{ClientAccount, Transaction, TransactionType};
 use anyhow::Result;
+use itertools::multizip;
 use polars::prelude::*;
+use std::collections::HashMap;
 use std::env;
 use std::path::Path;
-use itertools::multizip;
-use crate::structures::{Transaction, TransactionType};
 
 // I debated between this LazyFrame implementation and streaming with `csv-async`. This was far less
 // verbose and might actually tolerate very-large datasets.
@@ -16,11 +17,15 @@ use crate::structures::{Transaction, TransactionType};
 fn parse_csv(file_in: &str) -> Result<LazyFrame> {
     let schema = Schema::from_iter(vec![
         Field::new("type".into(), DataType::String),
-        Field::new("client".into(), DataType::UInt32),
+        Field::new("client".into(), DataType::UInt32), // Using U32 due to limitations on the CSV reader's functionality
         Field::new("tx".into(), DataType::UInt32),
         Field::new("amount".into(), DataType::Float64),
     ]);
-    Ok(LazyCsvReader::new(PlPath::new(file_in)).with_schema(Some(SchemaRef::from(schema))).with_has_header(false).with_skip_rows(1).finish()?)
+    Ok(LazyCsvReader::new(PlPath::new(file_in))
+        .with_schema(Some(SchemaRef::from(schema)))
+        .with_has_header(false)
+        .with_skip_rows(1)
+        .finish()?) // Skipping rows in order to compensate for the lack of a `with_clean_column_names` method for lazy readers
 }
 
 fn main() -> Result<()> {
@@ -39,12 +44,10 @@ fn main() -> Result<()> {
     // Don't need to drop, since it's lazy and is memory-light
     let lazy_data: LazyFrame = parse_csv(path.to_str().unwrap())?;
 
-
     // Partition by client to simplify downstream logic. Not required, and may not yield any performance improvement.
-    let parts = lazy_data
-        .collect()?
-        .partition_by(["client"], true)?;
+    let parts = lazy_data.collect()?.partition_by(["client"], true)?;
 
+    let client_accounts: HashMap<u32, ClientAccount> = HashMap::new(); // Master collection of accounts
 
     for df in &parts {
         println!("{:?}", df);
@@ -54,20 +57,23 @@ fn main() -> Result<()> {
         let columns = df.columns(["type", "client", "tx", "amount"])?;
 
         let type_col_iter = columns[0].str()?.iter();
-        let client_col_iter = columns[1].u32()?.iter();
+        let client_col_iter = columns[1].u32()?.iter(); // Using U32 due to limitations on the CSV reader's functionality
         let tx_col_iter = columns[2].u32()?.iter();
         let amount_col_iter = columns[3].f64()?.iter();
 
-        let full_row_iter = multizip((type_col_iter, client_col_iter, tx_col_iter, amount_col_iter));
+        let full_row_iter =
+            multizip((type_col_iter, client_col_iter, tx_col_iter, amount_col_iter));
 
         let transaction_objects: Vec<Transaction> = full_row_iter
-            .map(|(kind, client, tx, amount) | Transaction {
-                kind: TransactionType::try_from(kind.expect("")).expect(format!("Invalid transaction type: {:#?}", kind).as_str()),
-                client: client.expect(""),
+            .map(|(kind, client, tx, amount)| Transaction {
+                kind: TransactionType::try_from(kind.expect("Type may not be null"))
+                    .expect(format!("Invalid transaction type: {:#?}", kind).as_str()),
+                client: client.expect("client may not be null"),
                 amount: amount.expect(""),
                 tx: tx.expect(""),
                 state: None,
-            }).collect();
+            })
+            .collect();
 
         println!("{:#?}", transaction_objects)
     }
