3256321d056bbc391b520732ee336d76aa73fbe9
Unwize <aaronsigaldev@gmail.com>
2025-09-27 18:43:20 -0700

    Added some errors

---
+++ src/errors.rs
@@ -0,0 +1,22 @@
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum KrakenError {
+    #[error("IO Error")]
+    IO,
+
+    #[error("Invalid enum value error: {0}")]
+    Enum(String),
+
+    #[error("Dispute Chronology Error: Base: {0}, Attempt: {1}")]
+    DisputeChronoError(u32, u32),
+
+    #[error("Dispute State Error: {0}")]
+    DisputeStateError(String),
+
+    #[error("No Such Transaction Error: {0}")]
+    NoSuchTransactionError(String),
+
+    #[error("Error")]
+    Error,
+}
